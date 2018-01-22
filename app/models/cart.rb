6f26b83a2a75fef1be3fb5478ff9a47e181fa74f
chrisjamesr <chrisjames.r@gmail.com>
2018-01-21 23:04:17 -0500

    add #add_item, tests passing

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,15 +4,11 @@ class Cart < ActiveRecord::Base
   belongs_to :user
 
   def add_item(item_id)
-    if existing_line_item = self.line_items.find_by(:item_id => item_id)
-      # existing_line_item.assign_attributes(:quantity => existing_line_item.quantity += 1)
-      existing_line_item.quantity += 1
-      existing_line_item
-      # line_item = self.line_items.find_by(:item => existing_line_item.item)
-      # line_item.update(:quantity => self.quantity += 1)
-      # binding.pry
+    if line_item = self.line_items.find_by(:item_id => item_id)
+      line_item.quantity += 1
+      line_item
     else
-      line_item = self.line_items.build(:item => Item.find(item_id))
+      self.line_items.build(:item => Item.find(item_id))
     end       
   end
 
