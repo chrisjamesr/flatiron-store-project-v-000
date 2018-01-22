4298000059e6695240db10728d2a8d798490a4be
chrisjamesr <chrisjames.r@gmail.com>
2018-01-21 23:02:43 -0500

    add #add_item, tests passing

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,9 +3,17 @@ class Cart < ActiveRecord::Base
   has_many :items, :through => :line_items
   belongs_to :user
 
-  def add_item(item)
-    self.line_items.first_or_create(item: item)
-    
+  def add_item(item_id)
+    if existing_line_item = self.line_items.find_by(:item_id => item_id)
+      # existing_line_item.assign_attributes(:quantity => existing_line_item.quantity += 1)
+      existing_line_item.quantity += 1
+      existing_line_item
+      # line_item = self.line_items.find_by(:item => existing_line_item.item)
+      # line_item.update(:quantity => self.quantity += 1)
+      # binding.pry
+    else
+      line_item = self.line_items.build(:item => Item.find(item_id))
+    end       
   end
 
   def total
@@ -14,3 +22,13 @@ class Cart < ActiveRecord::Base
 
 
 end
+
+# def add_item(item_id)
+#     line_item = self.line_items.build(:item => Item.find(item_id))
+#     if self.items.include?(line_item.item)
+#       self.line_items.find_by(:item => line_item.item).quantity += line_item.quantity
+#     else
+#       save
+#     end       
+#   end
+# existing_line_item.assign_attributes({ :quantity => 1})
