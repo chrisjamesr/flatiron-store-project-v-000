38e4c1dded96a5540e750c3aad1bba2ac27b2e5a
chrisjamesr <chrisjames.r@gmail.com>
2018-01-21 20:35:47 -0500

    add associations, add #add_item..passing

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,16 @@
 class Cart < ActiveRecord::Base
+  has_many :line_items
+  has_many :items, :through => :line_items
+  belongs_to :user
+
+  def add_item(item)
+    self.line_items.first_or_create(item: item)
+    
+  end
+
+  def total
+     
+  end
+
 
 end
