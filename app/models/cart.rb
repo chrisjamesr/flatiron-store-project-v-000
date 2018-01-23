7b31a5e129fd077fac0dd7bdbe9cfe7c872fd7f4
chrisjamesr <chrisjames.r@gmail.com>
2018-01-22 20:37:07 -0500

    remove extraneous code

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -7,11 +7,9 @@ class Cart < ActiveRecord::Base
     if line_item = self.line_items.find_by(:item_id => item_id)
       line_item.quantity += 1
       line_item
-      # binding.pry
     else
       self.line_items.build(:item => Item.find(item_id))
     end       
-    # save
   end
 
   def total
@@ -22,15 +20,4 @@ class Cart < ActiveRecord::Base
     total      
   end
 
-
-end
-
-# def add_item(item_id)
-#     line_item = self.line_items.build(:item => Item.find(item_id))
-#     if self.items.include?(line_item.item)
-#       self.line_items.find_by(:item => line_item.item).quantity += line_item.quantity
-#     else
-#       save
-#     end       
-#   end
-# existing_line_item.assign_attributes({ :quantity => 1})
+end  #  End of Class
