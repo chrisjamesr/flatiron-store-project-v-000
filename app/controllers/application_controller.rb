e706d010540c652ce4110a2219699c15b85a9004
chrisjamesr <chrisjames.r@gmail.com>
2018-01-22 22:47:49 -0500

    create helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_cart, :current_user
+  # before_action :require_login
+  
 
+  # def current_user
+  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  # end
+
+  def current_cart
+    current_user.current_cart
+  end
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def require_login
+    redirect_to root_path unless logged_in?
+  end
 end
