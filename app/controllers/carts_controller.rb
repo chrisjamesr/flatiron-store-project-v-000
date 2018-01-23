8f2c6d2898d6f43037819d409cbf6398237a990a
chrisjamesr <chrisjames.r@gmail.com>
2018-01-22 22:50:01 -0500

    add empty create method

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,19 @@
 class CartsController < ApplicationController
+helper_method :current_cart
 
+  def index
+    @carts = Cart.where(:user => current_user)
+  end
+
+  def show
+    @cart = Cart.find(params[:id]) || current_cart
+  end
+
+  def create
+    
+  end
+
+
+
+ 
 end
