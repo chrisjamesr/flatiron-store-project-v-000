8f206adf9c1dcfe96d39b304e03a8417d07aa411
chrisjamesr <chrisjames.r@gmail.com>
2018-01-27 14:23:24 -0500

    complete create action

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,8 +1,14 @@
 class LineItemsController < ApplicationController
 
   def create
-    raise params.inspect
-    current_cart.add_item(params[:item_id])
+    current_user.create_current_cart unless current_cart
+    line_item = current_cart.add_item(params[:item_id])
+    if line_item.save
+      redirect_to cart_path(current_cart),  {:notice => "Added to Cart"}
+    else 
+      redirect_to store_path, {:notice => "Not Added to Cart"}   
+    end
   end
 
 end
+
