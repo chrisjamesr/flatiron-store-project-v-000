f3022fea327135d7959013bbc10d238a7dbfae07
chrisjamesr <chrisjames.r@gmail.com>
2018-01-27 15:20:25 -0500

    add checkout action with redirect

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,14 +6,16 @@ helper_method :current_cart
   end
 
   def show
-    @cart = Cart.find(params[:id]) || current_cart
+    @cart = current_cart || Cart.find(params[:id]) 
   end
 
-  def update
-    # @cart = Cart    
+  def checkout
+    current_cart.checkout
+    redirect_to cart_path(current_cart), {:notice => "Your order has been placed"}
+  
+    # set current_cart to nil
   end
 
-
+end
 
  
-end
