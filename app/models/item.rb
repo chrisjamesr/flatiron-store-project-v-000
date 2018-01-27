c358758b3e93a73d80e3458e923f1bb1a46ebfe6
chrisjamesr <chrisjames.r@gmail.com>
2018-01-27 14:30:25 -0500

    add associations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ActiveRecord::Base
   belongs_to :category
   has_many :line_items
+  belongs_to :line_item
+  has_many :users, :through => :line_items
 
   def self.available_items
     where('inventory > 0')   
