7a897c106829bb8ff2eb2a54817e99e11730a949
chrisjamesr <chrisjames.r@gmail.com>
2018-01-27 15:30:26 -0500

    add current_user method call to checkout action

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -12,7 +12,7 @@ helper_method :current_cart
   def checkout
     current_cart.checkout
     redirect_to cart_path(current_cart), {:notice => "Your order has been placed"}
-  
+    current_user.remove_current_cart
     # set current_cart to nil
   end
 
